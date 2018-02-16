732f7892d7866a225b09081aa2884a96df3e30e2
Ari <alk511@ad.nyu.edu>
2018-02-16 13:39:55 -0500

    2/16/18

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var map =
-    L.map('my-map').setView([40.713435,-73.971291], 11);
+    L.map('my-map').setView([40.731389,-73.992748], 11);
     // L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.{ext}', {
   	// attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   	// subdomains: 'abcd',
